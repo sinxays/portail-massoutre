089284007f092e51270cb2c3a75a8742ccd2497e
Sinxay Souvannavong <sinxay.souvannavong@gmail.com>
2022-12-05 13:19:35 +0100

    payplan

--- constantes/connectionDB_pdo.php
+++ constantes/connectionDB_pdo.php
@@ -22,7 +22,7 @@ class Connection
     public static function getPDO_2(): PDO
     {
         $user = 'root';
-        $password = 'M@ssoutre2013';
+        $password = 'root';
 
         return new PDO('mysql:host=localhost;dbname=massoutre', $user, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
