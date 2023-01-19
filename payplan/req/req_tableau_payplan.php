fca1627d93439c02403d746594834cdda4c3445f
Sinxay Souvannavong <sinxay.souvannavong@gmail.com>
2023-01-19 14:40:44 +0100

    payplan_19_01

--- payplan/req/req_tableau_payplan.php
+++ payplan/req/req_tableau_payplan.php
@@ -4,14 +4,5 @@ include  "../../include.php";
 
 
 $payplan = get_payplan();
-
-
-
 $table = create_table_payplan($payplan_table_header_row, $payplan);
-
-// var_dump($table);
-// die();
-
-
-
 echo $table;
