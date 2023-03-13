728beef3dd86afc6ede7c9e03ee859d2c9316d91
Sinxay Souvannavong <sinxay.souvannavong@gmail.com>
2023-03-13 11:08:42 +0100

    payplan_13_03_2023

--- payplan/req/update_payplan.php
+++ payplan/req/update_payplan.php
@@ -9,8 +9,8 @@ ini_set("display_errors", 1);
 // truncate_before_update();
 
 //dates personnalisées
-$filtre['date_personnalisee']['debut'] = "2022-09-01";
-$filtre['date_personnalisee']['fin'] = "2022-09-30";
+$filtre['date_personnalisee']['debut'] = "2022-08-01";
+$filtre['date_personnalisee']['fin'] = "2022-08-31";
 
 // mois précédent
 // $filtre['mois_precedent'] = array();
