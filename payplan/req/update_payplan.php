8cab8f97ff2146d26dd2c7a1222fb86ff28a57f5
Sinxay Souvannavong <sinxay.souvannavong@gmail.com>
2023-04-17 13:33:52 +0200

    payplan_17_04

--- payplan/req/update_payplan.php
+++ payplan/req/update_payplan.php
@@ -9,11 +9,11 @@ ini_set("display_errors", 1);
 // truncate_before_update();
 
 //dates personnalisées
-// $filtre['date_personnalisee']['debut'] = "2022-12-01";
-// $filtre['date_personnalisee']['fin'] = "2022-12-31";
+$filtre['date_personnalisee']['debut'] = "2022-06-01";
+$filtre['date_personnalisee']['fin'] = "2022-06-30";
 
 // mois précédent
-$filtre['mois_precedent'] = array();
+// $filtre['mois_precedent'] = array();
 
 //mois en cours
 // $filtre = '';
