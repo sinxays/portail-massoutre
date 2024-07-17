7caf94a44682d52f2824edb3004f2a4c209ab446
Sinxay SOUVANNAVONG <s.souvannavong@massoutre-locations.com>
2024-07-17 12:43:42 +0200

    update 17_07_2024

--- operations/shop_exterieurs/req/req_ajouter_shop_exterieur.php
+++ operations/shop_exterieurs/req/req_ajouter_shop_exterieur.php
@@ -5,7 +5,7 @@ include "../../../include.php";
 if (isset($_POST)) {
 
     try {
-        ajouter_shop_exterieur($POST);
+        ajouter_shop_exterieur($_POST);
     } catch (Exception $e) {
         // Capture de l'exception et gestion de l'erreur
         echo "Erreur : " . $e->getMessage();
