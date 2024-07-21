29b236788445695cf6715a50423285e104ea3e2d
Sinxay SOUVANNAVONG <s.souvannavong@massoutre-locations.com>
2024-07-22 01:27:32 +0200

    update 20_07_2024

--- test.php
+++ test.php
@@ -60,10 +60,10 @@
         ini_set('max_execution_time', 0);
 
         /****** UPDATE ALL ******/
-        update_vh_bdc_OS();
-        update_vh_factures_OS();
-        update_vh_invoice();
-        update_bdc_invoice();
+        // update_vh_bdc_OS();
+        // update_vh_factures_OS();
+        // update_vh_invoice();
+        // update_bdc_invoice();
 
         // delete_factures_restant();
         
