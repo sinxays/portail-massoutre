d1ed2abc1280b80db2f389428076dd8b86b86744
Sinxay Souvannavong <sinxay.souvannavong@massoutre-locations.com>
2024-08-23 11:39:27 +0200

    update_23_08_2024

--- pages_stats/req/req_alimenter_suivi_ventes.php
+++ pages_stats/req/req_alimenter_suivi_ventes.php
@@ -10,7 +10,7 @@ ini_set('max_execution_time', 0);
 if (isset($_POST['date']) && $_POST['date'] !== '') {
     $date = $_POST['date'];
 
-    $date = "2024-06-27";
+    // $date = "2024-06-27";
     update_bdc_canceled($date);
     update_factures_canceled($date);
     alimenter_suivi_ventes_bdc_via_portail($date);
