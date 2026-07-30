3d608472875af6a80eefe38719e72cf91631e345
Sinxay Souvannavong <sinxay.souvannavong@massoutre-locations.com>
2026-07-30 11:20:10 +0200

    modif du header

--- operations/shop_exterieurs/shop_exterieurs.php
+++ operations/shop_exterieurs/shop_exterieurs.php
@@ -25,25 +25,10 @@
 
 <body>
     <!--========== HEADER ==========-->
-    <header class="header">
-        <div class="header__container">
-            <img src="../../assets/img/perfil.jpg" alt="" class="header__img">
+    <?php include "../../header.php";
 
-            <a href="#" class="header__logo"> </a>
-
-            <div class="header__search">
-                <input type="search" placeholder="Search" class="header__input">
-                <i class='bx bx-search header__icon'></i>
-            </div>
-
-            <div class="header__toggle">
-                <i class='bx bx-menu' id="header-toggle"></i>
-            </div>
-        </div>
-    </header>
-
-    <!--========== NAV ==========-->
-    <?php include "../../right_menubar.php";
+    // NAV  
+    include "../../right_menubar.php";
     include "../../include.php"; ?>
 
 
